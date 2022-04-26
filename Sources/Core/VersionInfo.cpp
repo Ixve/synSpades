710fc8d507dc4066beab974fce8b05490df8313b
Cum <synthh@christiandior.anonaddy.com>
2022-04-27 00:03:59 +0200

    Update VersionInfo.cpp

--- Sources/Core/VersionInfo.cpp
+++ Sources/Core/VersionInfo.cpp
@@ -13,25 +13,25 @@
 
 std::string VersionInfo::GetVersionInfo() {
 #if defined(OS_PLATFORM_LINUX)
-	return std::string("synSpades - https://www.github.com/c-um/synSpades");
+	return std::string("synSpades - www.github.com/c-um/synSpades");
 #elif defined(TARGET_OS_MAC)
-	return std::string("Mac OS X | This user has compiled synSpades for macOS, GG! - https://www.github.com/c-um/synSpades");
+	return std::string("Mac OS X | This user has compiled synSpades for macOS, GG! - www.github.com/c-um/synSpades");
 #elif defined(OS_PLATFORM_WINDOWS)
 
 	std::string windowsVersion;
 
 	if (IsWindowsXPOrGreater() && !IsWindowsVistaOrGreater()) {
-		windowsVersion = "Windows XP | synSpades - Whoever is this player, is a psychopath. Who plays on XP these days. - https://www.github.com/c-um/synSpades";
+		windowsVersion = "Windows XP | synSpades - Whoever is this player, is a psychopath. Who plays on XP these days. - www.github.com/c-um/synSpades";
 	} else if (IsWindowsVistaOrGreater() && !IsWindows7OrGreater()) {
-		windowsVersion = "Windows Vista | synSpades - Why is this player using Vista.. - https://www.github.com/c-um/synSpades";
+		windowsVersion = "Windows Vista | synSpades - Why is this player using Vista.. - www.github.com/c-um/synSpades";
 	} else if (IsWindows7OrGreater() && !IsWindows8OrGreater()) {
-		windowsVersion = "Windows 7 | synSpades - Windows 7 Best Windows version no cap. - https://www.github.com/c-um/synSpades";
+		windowsVersion = "Windows 7 | synSpades - Windows 7 Best Windows version no cap. - www.github.com/c-um/synSpades";
 	} else if (IsWindows8OrGreater() && !IsWindows8Point1OrGreater()) {
-		windowsVersion = "Windows 8 | synSpades - https://www.github.com/c-um/synSpades";
+		windowsVersion = "Windows 8 | synSpades - www.github.com/c-um/synSpades";
 	} else if (IsWindows8Point1OrGreater()) {
-		windowsVersion = "Windows 8.1 | synSpades - https://www.github.com/c-um/synSpades";
+		windowsVersion = "Windows 8.1 | synSpades - www.github.com/c-um/synSpades";
 	} else {
-		windowsVersion = "Windows 10 | synSpades - https://www.github.com/c-um/synSpades";
+		windowsVersion = "Windows 10 | synSpades - www.github.com/c-um/synSpades";
 	}
 
 	if (IsWindowsServer())
